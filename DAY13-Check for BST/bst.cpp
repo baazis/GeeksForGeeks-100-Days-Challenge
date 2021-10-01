7b92d05fe4d9e11a98f0103c6b513f89ef126683
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-10-01 09:43:40 +0530

    Update bst.cpp

--- DAY13-Check for BST/bst.cpp
+++ DAY13-Check for BST/bst.cpp
@@ -7,7 +7,7 @@ class Solution
     {
         if(root==NULL)
             return true;
-            
+         //recursively checking that teh lefts node data is less that the root and its right node is greater than its root.   
         if(root->data>min && root->data<max
         && IsBST(root->left,min,root->data)
         && IsBST(root->right,root->data,max)) 
