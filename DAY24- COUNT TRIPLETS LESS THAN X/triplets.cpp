26e7db723be8c0b32f1f48cabc9f88f80a019ce2
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2021-10-26 23:13:21 +0530

    Add files via upload

---
+++ DAY24- COUNT TRIPLETS LESS THAN X/triplets.cpp
@@ -0,0 +1,25 @@
+class Solution{
+	public:
+	long long countTriplets(long long arr[], int n, long long sum)
+	{
+    	int l,r;
+        long int s=0;
+        long int count=0;
+        sort(arr,arr+n);
+        for(int i=0;i<n;i++){
+            l=i+1;
+            r=n-1;
+            while(l<r){
+                s=arr[i]+arr[l]+arr[r];
+                if (s>=sum){
+                    r--;
+                }
+                else{
+                    count+=r-l;
+                    l++;
+                }
+            }
+        }
+        return count;
+	}
+};
